e618f514e5b2f0d6cacb027f87474498d3ecba4e
nater <n.rome26@gmail.com>
2023-06-26 15:59:15 -0700

    added the input field and button for the post with styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+"use client"
+
+import AddPost from './components/AddPost'
 
 export default function Home() {
   return (
     <main>
       <h1>Hello Next</h1>
+      <AddPost />
     </main>
   )
 }
